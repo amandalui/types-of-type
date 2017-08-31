5d9752f006e797799235aff293dbe617c1122d0c
amandalui <hi.amandalui@gmail.com>
2017-08-31 00:07:33 -0700

    Initial framwork

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+  <meta charset="UTF-8">
+  <meta name="description" content="Types of Type" />
+  <meta name="keywords" content="types of type, typography, korean, 타이포그래픽, 타이포그라피, 타이포그래피, 타이포, 한글, graphic design, designer, amanda lui" />
+  <meta name="author" content="Amanda Lui" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta http-equiv="cache-control" content="max-age=3600" />
+  <meta name="viewport" content="initial-scale=1">
+
+    <link rel="SHORTCUT ICON" href="images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Bitter|Noto+Sans:400,700|Work+Sans:300,400,700" rel="stylesheet">
---
+++ includes/header-nav.php
@@ -0,0 +1,5 @@
+  <nav>
+    <div class="item"><a href="index.html">TYPES OF TYPE</a></div>
+    <div class="item center"><img src="images/nav-button.png"></div>
+    <div class="item right"><a href="about.html">About / Credits</a></div>
+  </nav>
