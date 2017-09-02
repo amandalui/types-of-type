7e9a5849f641faaeae86060cfe3c18f0aec81638
amandalui <hi.amandalui@gmail.com>
2017-09-02 01:24:16 -0700

    Hangul Blocks
    Sections 1 and 2

---
+++ hangul-blocks.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Types of Type | Hangul Blocks</title>
+  <?php include('includes/head.php'); ?>
+</head>
+    
+<body>
+
+  <!-- TOP NAV -->
+  <?php include('includes/header-nav.php'); ?>
+    
+  <!-- HEADER -->
+  <div class="header-wrap clear">
+      <div class="header-container">
+        <div class="header-section-title caps">
+          The Basics
+        </div>
+        <div class="header-content">
+          <span class="header-section-number">1&ndash;2</span>
+          <h1>Hangul Blocks</h1>
+          <p class="header-section-desc">Making words is like playing Tetris&mdash;from the start Korean letters were created modularly.</p>
+        </div>
+      </div>
+  </div>
+
+  <!-- CONTENT -->
+  <div class="content blue clear">
+    <div class="column--single spacer--small">
+      <div class="column--single center img--100"><img src="images/hangul-blocks/slide-1.svg" alt="Hangul Blocks"></div>
+      <h3 class="spacer--small caps center text--blue">(I am not Korean)</h3>
+    </div>
+    <div class="spacer--small"></div>
+  </div>
+  <div class="content mediumblue spacer--medium">
+    <div class="column--single text--lightpink rule img--100">
+      <h2 class="center caps">Consonants are Square</h2>
+      <p class="serif center">Based on the physical forms of the tongue, mouth, and throat.</p>
+      <div class="flex--center">
+        <div class="column--one-fourth spacer--medium"><img src="images/hangul-blocks/consonants.gif" alt="Hangul Consonants"></div>
+        <div class="column--three-fourths spacer--medium"><img src="images/hangul-blocks/consonants-array.svg" alt="Hangul Consonants Array"></div>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="column--single text--yellow img--100 spacer--medium">
+      <h2 class="center caps">Vowels are Rectangular</h2>
+      <p class="serif center">Based on the harmony of the earth, yin (earth) and yang (sky).</p>
+      <div class="flex--center">
+        <div class="column--one-fourth spacer--medium"><img src="images/hangul-blocks/vowels.gif" alt="Hangul Vowels"></div>
+        <div class="column--three-fourths spacer--medium"><img src="images/hangul-blocks/vowels-array.svg" alt="Hangul Vowels Array"></div>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+  <div class="content lightblue">
+  
+  <!-- FOOTER -->
+  <?php include('includes/footer.php'); ?>
+  </div> 
+</body>
+</html>
