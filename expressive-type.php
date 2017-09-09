db99624b639c82c65e8f4ebe43b0e11d8da76043
thisjeremiah <jeremiahmontoya@me.com>
2017-09-09 15:09:42 -0700

    Rewrite expressive type to use vue rendering

--- expressive-type.php
+++ expressive-type.php
@@ -37,98 +37,27 @@
   </div>
 
   <!-- CONTENT -->
-  <div class="content--first lightpink text--darkgreen clear">
-    <div class="column--single img--100">
-    <div class="column--single">
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/01-01-en-playground.svg" alt="Playground"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Sofia Soft</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/01-01-kr-playground.svg" alt="Playground"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">BM Jua</span>
-                </div>
-            </div>
-            <div class="clear rule spacer--medium"></div>
-        </div>
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/02-01-en-dearfriend.svg" alt="Dear Friend"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Kinescope</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/02-01-kr-dearfriend.svg" alt="Friendly"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Pocheon Makgeoli</span>
-                </div>
-            </div>
-            <div class="clear rule spacer--medium"></div>
-        </div>
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/03-01-en-volcano.svg" alt="Volcano"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Freckle Face</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/03-01-kr-volcano.svg" alt="Volcano"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Jeju Hallasan</span>
-                </div>
-            </div>
-            <div class="clear rule spacer--medium"></div>
-        </div>
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/04-01-en-environmental.svg" alt="Environmental"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Amatic</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/04-01-kr-environmental.svg" alt="Environmental"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Sangsangtokki Blossom</span>
-                </div>
-            </div>
-            <div class="clear rule spacer--medium"></div>
-        </div>
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/05-01-en-seventies.svg" alt="Seventies"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Grad</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/05-01-kr-seventies.svg" alt="Seventies"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Bom Param</span>
-                </div>
-            </div>
-            <div class="clear rule spacer--medium"></div>
-        </div>
-        <div class="spacer--small">
-            <div class="expressive-wrapper spacer--medium">
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/06-01-en-historical.svg" alt="Historical"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">Givry</span>
-                </div>
-                <div class="column--half center spacer--small">
-                    <img src="/images/expressive-type/06-01-kr-historical.svg" alt="Historical"><br /><span class="text--brown caption caps">Typeface</span><br /><span class="caps bold">N/A</span>
-                </div>
-            </div>
-            <div class="clear"></div>
-        </div>
-            <div class="spacer--small clear"></div>
-
-        </div></div>
+    <div class="content--first lightpink text--darkgreen clear">
+      <?php include('includes/expressive-type-animations.php') ?>
         
-    <div class="clear"></div>
-    <div class="spacer--medium center link--end">
-        <a class="caps" href="/about.php">About / Credits</a>
-    </div>  
+      <div class="clear"></div>
+      <div class="spacer--medium center link--end">
+          <a class="caps" href="/about.php">About / Credits</a>
+      </div>  
       
-  <!-- NEXT SECTION -->
+      <!-- NEXT SECTION -->
 
-  <!-- FOOTER -->
-  <footer class="caption text--darkpink">
-      <div class="center">&copy; 2013&ndash;<script language="JavaScript" type="text/javascript">
-            now = new Date
-            theYear=now.getYear()
-            if (theYear < 1900)
-            theYear=theYear+1900
-            document.write(theYear)
-          </script> Amanda Lui
-      </div>
-  </footer>
-  </div> 
-    
-</body>
+      <!-- FOOTER -->
+      <footer class="caption text--darkpink">
+          <div class="center">&copy; 2013&ndash;<script language="JavaScript" type="text/javascript">
+                now = new Date
+                theYear=now.getYear()
+                if (theYear < 1900)
+                theYear=theYear+1900
+                document.write(theYear)
+              </script> Amanda Lui
+          </div>
+      </footer>
+    </div> 
+  </body>
 </html>
