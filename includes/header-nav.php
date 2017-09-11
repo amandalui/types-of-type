96b2e672f523a3ad1f7459fcb777b78e0ea4a8c0
amandalui <hi.amandalui@gmail.com>
2017-09-10 21:45:31 -0700

    Dropdown menu styling

--- includes/header-nav.php
+++ includes/header-nav.php
@@ -1,45 +1,5 @@
-<style>
-  [v-cloak] {
-    display: none;
-  }
-  .pointer {
-    cursor: pointer;
-  }
-  .relative {
-    position: relative;
-  }
-  .z4 { z-index: 4 }
-  .z0 { z-index: 0 }
-  .dropdown {
-    width: 80vw;
-    left: -24vw;
-    position: absolute;
-    top: 50px;
-    height: 100vh;
-    background-color: #333;
-    transition: opacity 300ms;
-    
-  }
-  @media (max-width: 768px) {
-    .dropdown {
-      width: 90vw;
-      left: -29vw;
-    }
-  }
-  .drop-enter-active, .drop-leave-active {
-    transition: top .3s
-  }
-  .drop-enter, .drop-leave-to {
-    top: -100vh
-  }
-</style>
-
 <div id="header">
-  <nav class="z4">
-    <div class="column--one-third"><a href="/"><img class="nav__logo" src="images/types-of-type-horizontal.svg"></a></div>
-    <div class="column--one-third center relative">
-      <img class="pointer" @click="toggleMenu" src="images/nav-button.png">
-      <transition name="drop">
+         <transition name="drop">
         <div
           class="dropdown"
           v-if="menuIsOpen"
@@ -48,6 +8,10 @@
           <?php include('includes/menu.php'); ?>
         </div>
       </transition>
+  <nav class="z4">
+    <div class="column--one-third"><a href="/"><img class="nav__logo" src="images/types-of-type-horizontal.svg"></a></div>
+    <div class="column--one-third center relative">
+      <img class="pointer" @click="toggleMenu" src="images/nav-button.png">
     </div>
     <div class="column--one-third right"><a href="/about.php">About / Credits</a></div>
   </nav>
