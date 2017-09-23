85b75e91d599374bf9914c6cac2a9734684b6d21
Nathan Specht <nathanspecht@gmail.com>
2017-09-23 12:08:35 -0700

    Add radial progress dots

--- src/components/HangulBlocksSlides.vue
+++ src/components/HangulBlocksSlides.vue
@@ -1,6 +1,16 @@
 <template>
 <div id="hangul-blocks-slides" class="content--first blue clear">
   <div class="column--single spacer--small">
+    <div class="progress-dots-container">
+      <div class="cf progress-dots">
+        <div
+          v-for="slide in slides"
+          :class="{ active: slideNum === slide }"
+          class="progress-dot"
+        >
+        </div>
+      </div>
+    </div>
     <div
       class="column--single center img--100 relative transition-opacity"
       @click="nextSlide()"
@@ -36,6 +46,45 @@
 </script>
 
 <style scoped>
+  .cf:before,
+    .cf:after   { content: " "; display: table; }
+    .cf:after   { clear: both; }
+    .cf         { *zoom: 1; }
+
+  .progress-dots-container {
+    margin-bottom: 40px;
+  }
+
+  .progress-dots {
+    float: right;
+    position: relative;
+    left: -50%;
+  }
+
+  .progress-dot {
+    float: left;
+    height: 10px;
+    width: 10px;
+    border-radius: 100%;
+    background-color: #047272;
+    position: relative;
+    left: 50%;
+    margin-right: 40px;
+
+    transition: background-color 0.6s linear;
+    -moz-transition: background-color 0.6s linear;    /* FF3.7+ */
+    -o-transition: background-color 0.6s linear;      /* Opera 10.5 */
+    -webkit-transition: background-color 0.6s linear; /* Saf3.2+, Chrome */
+  }
+
+  .progress-dot:last-of-type {
+    margin-right: 0px;
+  }
+
+  .progress-dot.active {
+    background-color: white;
+  }
+
   .relative {
     position: relative;
   }
