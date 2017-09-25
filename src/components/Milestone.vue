0e8e1c9e61c298b5a6a76ee42295b83dc583dc53
Nathan Specht <nathanspecht@gmail.com>
2017-09-24 20:02:21 -0700

    Add scroll animation for timeline

---
+++ src/components/Milestone.vue
@@ -0,0 +1,68 @@
+<template>
+    <div
+      ref="el"
+      :class="className"
+    >
+      <div :class="`content-timeline ${milestone.color}`">
+        <div class="caps bottom-br">
+          <span class="serif">{{ milestone.year }}</span>
+          <span class="bold">{{ milestone.title }}</span>
+        </div>
+        <br />
+        <img :src="`../images/origins-of-type/${milestone.img}`" :alt="`${milestone.alt}`">
+        <p class="caption serif">{{ milestone.caption }}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'milestone',
+    props: {
+      scrollBottom: Number,
+      milestone: Object,
+    },
+    data () {
+      return {
+        offsetTop: null,
+        hasActivated: false,
+      }
+    },
+    mounted () {
+      this.offsetTop = this.$refs.el.getBoundingClientRect().top
+    },
+    computed: {
+      className: function () {
+        return [
+          'container-timeline',
+          `${this.milestone.side}-timeline`,
+          `slide-fade-enter${this.isActive() ? '-active' : `-${this.milestone.side}`}`,
+        ]
+      }
+    },
+    methods: {
+      isActive () {
+        if (this.hasActivated) return true
+        if (typeof this.offsetTop !== 'number') return false
+        const isInView = this.offsetTop < this.scrollBottom
+        if (isInView) this.hasActivated = true
+        return isInView
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .slide-fade-enter-active {
+    transition: all 1s ease;
+  }
+  .slide-fade-enter-right {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+  .slide-fade-enter-left {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+</style>
+
---
+++ src/components/Timeline-old.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <div class="timeline text--blue img--100">
+      <h3 class="column--half caps center timeline-descriptor">Korean</h3>
+      <h3 class="column--half caps center timeline-descriptor">English</h3>
+      <div class="clear"></div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+              <span class="serif">4000 BC</span>
+              <span class="bold">Cuneiform</span>
+            </div>
+            <br />
+            <img src="../images/origins-of-type/01-cuneiform.svg" alt="Cuneiform">
+            <p class="caption serif">Sumerians in Mesopotamia created pictograms called Cuneform.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">2333 BC</span>
+                <span class="bold">Hanja</span>
+            </div>
+            <br />
+            <img src="../images/origins-of-type/02-hanja.svg" alt="Hanja">
+            <p class="caption serif">The Chinese writing system was used initially when Gojoseon (modern day Korea) was founded.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+          <div class="caps bottom-br">
+            <span class="serif">3000 BC</span>
+            <span class="bold">Hieroglyphs</span>
+          </div>
+          <br />
+          <img src="../images/origins-of-type/03-hieroglyphs.svg" alt="Hieroglyphs">
+          <p class="caption serif">Egyptians developed pictograms called Hieroglyphs. Each Hieroglyph was a visual representation of a word.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+          <div class="caps bottom-br">
+            <span class="serif">1600 BC</span>
+            <span class="bold">Phoenician</span>
+          </div>
+          <br />
+          <img src="../images/origins-of-type/04-phoenician.svg" alt="Phoenician">
+          <p class="caption serif">Phoenicians in Egypt developed an alphabet where characters represented sound rather than objects.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+          <div class="caps bottom-br">
+            <span class="serif">800 BC</span>
+            <span class="bold">Greek &amp; Roman</span>
+          </div><br />
+          <img src="../images/origins-of-type/05-greek-roman.svg" alt="Greek and Roman">
+          <p class="caption serif">Greeks adopted Phoenician and added a few vowels. Then, the Romans refined it to create the alphabet we use today.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">950 BC</span> <span class="bold">Idu</span>
+            </div><br />
+            <img src="../images/origins-of-type/06-idu.svg" alt="Idu">
+            <p class="caption serif">Modified Hanja with simplified characters representing Korean words and sounds. Idu combines the Hyangchai and Gugyeol systems of writing.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1000</span> <span class="bold">Carolingian</span>
+            </div><br />
+            <img src="../images/origins-of-type/07-carolingian.svg" alt="Carolingian">
+            <p class="caption serif">The King of Italy, Charlemagne, could not handle disorder and hired a man from York to create orderly, legible, type with upper/lower csae letters; it was called Carolingian script.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">1446</span> <span class="bold">Hangul</span>
+            </div><br />
+            <img src="../images/origins-of-type/08-hangul.svg" alt="Hangul">
+            <p class="caption serif">King Sejong created the original Korean letters and released them in his speech Hunmin Jeong-eum (The Proper Sounds for the Education of the People).</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1500</span> <span class="bold">Metal Type</span>
+            </div><br />
+            <img src="../images/origins-of-type/09-metal-type.svg" alt="Metal Type">
+            <p class="caption serif">The invention of Gutenberg printing press and metal type initiated the revolution of machine made typography.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">1504</span> <span class="bold">Yeonsangun</span>
+            </div><br />
+            <img src="../images/origins-of-type/10-yeonsangun.svg" alt="Yeonsangun">
+            <p class="caption serif">The new Hangul letters were so easy to learn that the aristocrats feared their corruption would be revealed, and their power taken away, by the newly literate. King Yeonsangun joined this opposition and banned Hangul due to it’s use by the lower class to ridicule him.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1600</span> <span class="bold">Renaissance</span>
+            </div><br />
+            <img src="../images/origins-of-type/11-renaissance.svg" alt="Renaissance">
+            <p class="caption serif">The French designed a simplified style of type with less influence from the scribe’s handwriting that its predecessors.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">1894</span> <span class="bold">Hangul Revival</span>
+            </div><br />
+            <img src="../images/origins-of-type/12-hangul-revival.svg" alt="Hangul Revival">
+            <p class="caption serif">Hangul was officially used again after going through a revival 100 years prior. Bourgeois used Hanja as an attempt to keep the gap between classes.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1800</span> <span class="bold">Realist</span>
+            </div><br />
+            <img src="../images/origins-of-type/13-realist.svg" alt="Realist">
+            <p class="caption serif">William Caslon IV started the trend of the sans serif, it quickly caught on. The sans serif was a simplified style that rid of the serifs at the ends of each letter.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">1910</span> <span class="bold">Japanese Colonization</span>
+            </div><br />
+            <img src="../images/origins-of-type/14-japan.svg" alt="Japanese Colonization">
+            <p class="caption serif">Japan colonized Korea and implemented Japanese as the official language. Hangul was officially banned thirty-one years later due to culterual assimilation.</p>
+        </div>
+      </div>
+      <div class="container-timeline right-timeline">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1920</span> <span class="bold">Modernist</span>
+            </div><br />
+            <img src="../images/origins-of-type/15-modernist.svg" alt="Modernist">
+            <p class="caption serif">Modernist typography was popularized in Germany at the Bauhaus. This tyle of type was influenced by geometric forms.</p>
+        </div>
+      </div>
+      <div class="container-timeline left-timeline">
+        <div class="content-timeline yellow">
+            <div class="caps bottom-br">
+                <span class="serif">1946</span> <span class="bold">Korean Independence</span>
+            </div><br />
+            <img src="../images/origins-of-type/16-korean-independance.svg" alt="Korean Independence">
+            <p class="caption serif">Korea regained independence and Hangul once again became the official written language. Three years after, North Korea banned the use of Hanja.</p>
+        </div>
+      </div>
+    </div>
+      <div class="container-timeline right-timeline img--100">
+        <div class="content-timeline lightpink">
+            <div class="caps bottom-br">
+                <span class="serif">1980</span> <span class="bold">Post Modernist</span>
+            </div><br />
+            <img src="../images/origins-of-type/17-post-modernist.svg" alt="Modernist">
+            <p class="caption serif">Post-modern type emerged as a reaction against the Modernist movement. The invention of the computer allowed for the creation of the unconventional.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'timeline'
+  }
+</script>
+
