20de5c699ace2d279549723649e532ce682f668c
amandalui <hello@amandalui.com>
2023-08-20 17:23:18 -0700

    UI Updates
    • Simplify home page UI
    • List format navigation
    • Clean up origins of type timeline UI

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
-    <div class="header-index center">
-      <img src="/static/images/index/types-of-type-index-logo.svg">
+    
+    <div class="hero--wrap">
+      <div class="hero--left">
+        <img src="/static/images/index/types-of-type-index-logo.svg">
+      </div>
+      <div class="hero--right">
+        <p class="hero--text">A guide to typography that compares English and Korean typeforms.</p>
+        <!-- <p class="caption">(No Korean language knowledge needed)</p> -->
+        <div class="spacer--small"></div>
+        <p>
+          <router-link class="button caps" to="/origins-of-type">Start Exploring &rarr;</router-link>
+        </p>
+      </div>
     </div>
 
-    <div class="intro-index-wrap center light spacer--medium">
-      <div class="intro-index">
-          <h3 class="caps">A guide to typography with character</h3>
-          <p>The art form of typography comparing English and Korean.</p>
-      </div>
-          <p class="caps caption text--lightgray">(No Korean language knowledge needed)</p>
-          <p class="spacer--tiny"></p>
-      <p class="spacer--medium"><router-link class="button caps" to="/origins-of-type">Start Exploring</router-link></p>
-    </div>
-      
     <div class="dark">
         <div class="spacer--medium"></div>
-        <h3 class="caps center text--light">Contents</h3>
-        <div class="spacer--tiny"></div>
         <app-menu></app-menu>
 
         <div class="spacer--large clear"></div>
